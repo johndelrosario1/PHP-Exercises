80c1605cc51181f65173dd83b0c69442f260881b
M307user <M307user@lab.uc-bcf.local>
2025-10-01 11:18:26 +0800

    Introduction

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Exercises</title>
+</head>
+<body>
+    <ul>
+        <li> <a href="intro.php">Introduce Yourself  </a></li>
+        <li><a href="math.php"></a></li>
+    </ul>
+<style>
+  body{
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    background-color: forestgreen;
+    text-align: center;
+    padding: 50px;
+  }
+</style>
+</body>
+</html>
---
+++ intro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction</title>
+</head>
+
+<body>
+<h2>Task 1. Introduce yourself</h2>
+<?php
+    $name="John Manuel C. Del Rosario";
+     $age='20';
+
+     echo "Hi, my name is" . $name;
+     echo "I am". $age; "years old";
+ ?>
+</body>
+</html>
